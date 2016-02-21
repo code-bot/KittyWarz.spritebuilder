ee8ca2fe3dfd1ae989ddd71b119e8e4892091cd0
karanlakhani <karanlakhani17@gmail.com>
2016-02-21 05:23:46 -0500

    Added clan/location

--- Source/ClanInfoScene.swift
+++ Source/ClanInfoScene.swift
@@ -7,9 +7,31 @@
 //
 
 import Foundation
+import Firebase
 
 class ClanInfoScene: CCNode {
     
+    weak var ninjaKills: CCLabelTTF!
+    weak var pirateKills: CCLabelTTF!
+    
+    func didLoadFromCCB() {
+        myRootRef.childByAppendingPath("ninjasKillCount").observeSingleEventOfType(.Value, withBlock: {
+            snapshot in
+            let ninjasKillCount = snapshot.value
+            self.ninjaKills.string = String(ninjasKillCount)
+            print(ninjasKillCount)
+        })
+        myRootRef.childByAppendingPath("piratesKillCount").observeSingleEventOfType(.Value, withBlock: {
+            snapshot in
+            let piratesKillCount = snapshot.value
+            self.pirateKills.string = String(piratesKillCount)
+            print(piratesKillCount)
+        })
+    }
+    
+    func goToLocationScene() {
+        CCDirector.sharedDirector().replaceScene(CCBReader.loadAsScene("LocationScene"))
+    }
     
     func goToBattle() {
         CCDirector.sharedDirector().replaceScene(CCBReader.loadAsScene("PreviewBattleScene"))
